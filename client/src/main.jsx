18827724a1c34beb379e96955ea1180cb163da1e
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-11-25 14:38:59 +0530

    Update Router & Add Redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,16 +3,42 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App';
+import DashboardLayout from './Dashboard/DashboardLayout';
+import Authorization from './Authorization/Authorization';
+import Main from './Dashboard/main/Main';
+import Workspace from './Dashboard/workspace/Workspace';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />
+  },
+  {
+    path: "/Dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "",
+        element: <Main />
+      },
+      {
+        path: "workspace",
+        element: <Workspace />
+      },
+      {
+        path: "access-control",
+        element: <Authorization />
+      }
+    ]
   }
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
