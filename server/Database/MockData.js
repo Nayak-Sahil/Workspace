acfbab244a47bbb51ab2b42b4681641326602bd7
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-11-25 23:15:53 +0530

    Add MockData: Shared Data

---
+++ server/Database/MockData.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+        id: 1,
+        username: 'nayak_sahil',
+        email: 'nayaksahild@gmail.com',
+        password: 'sahil123',
+        role: 'admin',
+        status: 'active',
+        createdAt: '2021-08-01T00:00:00.000Z',
+    },
+    {
+        id: 2,
+        username: 'john_doe',
+        email: 'john@gmail.com',
+        password: 'john',
+        role: 'Editor',
+        status: 'active',
+        createdAt: '2021-08-01T00:00:00.000Z',
+    },
+    {
+        id: 3,
+        username: 'roy',
+        email: 'roy@gmail.com',
+        password: 'roy',
+        role: 'Viewer',
+        status: 'active',
+        createdAt: '2021-08-01T00:00:00.000Z',
+    },
+]
+
+module.exports = data;
