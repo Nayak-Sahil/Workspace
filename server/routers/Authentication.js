8a37eaace2d70a6f9986c9f0c0811b4538dc78e4
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-11-27 18:52:35 +0530

    Update Cookie-Configuration: httpOnly

--- server/routers/Authentication.js
+++ server/routers/Authentication.js
@@ -22,7 +22,7 @@ AuthRouter.post('/login', [ValidateToken], (req, res) => {
     } else {
         const token = jwt.sign({ ...user }, process.env.JWT_SECRET, { expiresIn: '1d' });
         try{
-            res.cookie('WS_TOKEN', token, {sameSite: 'None', secure: true, httpOnly: false, path: '/'});
+            res.cookie('WS_TOKEN', token, {sameSite: 'None', secure: true, httpOnly: true, path: '/'});
         }catch(err){
             console.log(err);
         }
